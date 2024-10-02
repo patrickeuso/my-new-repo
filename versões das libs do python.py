b345c4b1327113896f094572ef1ca0ce7a88ba15
patrickeuso <patrickeuso@hotmail.com>
2024-10-02 16:05:41 -0300

    exclusão de arquivos desnecessários

--- versões das libs do python.py
+++
@@ -1,7 +0,0 @@
-import importlib.metadata
-
-libraries = ['pandas', 'scipy', 'streamlit','plotly']
-versions = {lib: importlib.metadata.version(lib) for lib in libraries}
-
-for lib, version in versions.items():
-    print(f'{lib}=={version}')
